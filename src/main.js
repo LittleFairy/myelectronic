4f4a29c2e0be9bb741ba1b34d9bb38454d2316c0
Desiderata <1065250727@qq.com>
2020-11-03 16:10:11 +0800

    init myelectronic

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 导入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入less
+import less from 'less'
+Vue.use(less)
+
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 配置请求的跟路径
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
